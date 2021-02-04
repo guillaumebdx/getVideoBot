5c8773bebcde101ade29583eab99da8495e550c9
Guillaume HARARI <guillaumeharari@hotmail.com>
2021-02-04 12:06:03 +0100

    Delete url from messages

--- src/Service/AnswerService.php
+++ src/Service/AnswerService.php
@@ -13,7 +13,7 @@ class AnswerService extends AbstractAuth
     private function generateMessage($username)
     {
 
-        $messages = [
+        /*$messages = [
             'Ok ' . $username . ' tu trouveras ton fichier tout beau tout propre ici : ' . self::URL . $username,
             'Wsh, ton fichier a bien été téléchargé ici ' . self::URL . $username,
             'GG ' . $username . ' t\'as un nouveau média dans ton espace : ' . self::URL . $username,
@@ -24,6 +24,18 @@ class AnswerService extends AbstractAuth
             'T\'as tout compris frr, tu me mentiones, je te mets ça au chaud : ' . self::URL . $username,
             'Voici ton lien bg : ' . self::URL . $username,
             'Avoue je te met bien avec un lien tout neuf pour toi : ' . self::URL . $username,
+        ];*/
+
+        $messages = [
+            'Ok ' . $username . ' tu trouveras ton fichier tout beau tout propre en suivant les consignes sur mon tweet épinglé',
+            'Wsh ' . $username . ', ton fichier a bien été téléchargé. Suis les consignes sur mon tweet épinglé',
+            'GG ' . $username . ' t\'as un nouveau média dans ton espace. Pour y accéder, va voir mon tweet épinglé',
+            'C\'est fait ' . $username . ' ! En gros, ton fichier est sauvegardé. Pour le voir, suis les consignes de mon tweet épinglé',
+            'Ok ' . $username . ', j\'ai posé ça sur ton espace, Pour y accéder, va voir mon tweet épinglé,  et oublie pas de me follow frérot',
+            'Yo ' . $username . ', j\'ai téléchargé ça rien que pour toi bg. Suis les consignes sur mon tweet épinglé',
+            'Ok bg/blg ton média est bien téléchargé, maintenant, ' . $username . ', va voir mon tweet épinglé pour le récupérer',
+            'Salut ' . $username . ' ! T\'as tout compris frr, tu me mentiones, je te mets ça au chaud. Pour y accéder, va voir mon tweet épinglé',
+            'Avoue ' . $username . ' je te met bien avec le fichier tout neuf pour toi : Va voir mon tweet épinglé pour le récupérer',
         ];
         return $messages[array_rand($messages)];
     }
